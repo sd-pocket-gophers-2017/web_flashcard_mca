3cc8dd78dba6c1286fc813e82bbdcd1c821ff522
Alexis DeRuyter <email": null>
2017-08-11 18:39:21 -0700

    Results page works correctly

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -12,11 +12,23 @@ post '/decks/:id' do
   unanswered_cards = cards.select { |card| card.guesses.empty? }
   if unanswered_cards.any?
     card = unanswered_cards.sample
+    redirect "/cards/#{card.id}"
   else
     incorrect_cards = cards.select do |card|
       card.guesses.select { |guess| guess.is_correct == 't' }.empty?
     end
-    card = incorrect_cards.sample
+    if incorrect_cards.any?
+      card = incorrect_cards.sample
+      redirect "/cards/#{card.id}"
+    else
+      redirect "/decks/#{params[:id]}/results"
+    end
   end
-  redirect "/cards/#{card.id}"
+end
+
+get '/decks/:id/results' do
+  @deck = Deck.find(params[:id])
+  grouped_guesses = @deck.guesses.group(:card_id).count(:guesses)
+  @first_try = grouped_guesses.select { |card, guess_count| guess_count == 1 }.count
+  slim :'decks/results'
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,7 @@
 class Deck < ApplicationRecord
   # Remember to create a migration!
   has_many :cards
+  has_many :guesses, through: :cards
 
   validates :name, presence: true, uniqueness: true
 end
